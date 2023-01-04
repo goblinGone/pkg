2bfbfde8dbb34fd51d6418a5bfd41637b8b16f1e
Status <ware.j@protonmail.com>
2023-01-04 17:42:45 +0000

    fixed the pkg
    now spits out a 13 char passkey
    although janky as fuck as it assigns a random vector to a set of 'parts' which i hate, but i was unable to get the vectors to play nice

--- main.cpp
+++ main.cpp
@@ -10,116 +10,41 @@
  ***************************************************************************/
 
 #include <iostream>
+#include <vector>
 #include <ctime>
-#include <vector>
-#include <random>
-#include <vector>
-#include <sstream>
-#include <string>
 
 using namespace std;
 
+int length = 13;
+
 vector<char> passkey;
 
-vector <char> myChars {'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n',
-'m','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M',
-'!','@','#','$','%','^','&','*','?','<','>','1','2','3','4','5','6','7','8','9','0'};
-
-void helpCommand() {
-    cout<< "\n" << "Help page for the Passkey Generator"<< endl;
-    cout<<"Currently the options are:"<< endl;
-    cout<<"H - Display the help stuff (this bit)"<< endl;
-    cout<<"C - Only use numbers and letters, ie no special chars"<< endl;
-    cout<<"A - Display the version and licencing stuff "<< endl;
-}
-
-void art() {
-    cout << R"(
-          _____                    _____                    _____          
-         /\    \                  /\    \                  /\    \         
-        /::\    \                /::\____\                /::\    \        
-       /::::\    \              /:::/    /               /::::\    \       
-      /::::::\    \            /:::/    /               /::::::\    \      
-     /:::/\:::\    \          /:::/    /               /:::/\:::\    \     
-    /:::/__\:::\    \        /:::/____/               /:::/  \:::\    \    
-   /::::\   \:::\    \      /::::\    \              /:::/    \:::\    \   
-  /::::::\   \:::\    \    /::::::\____\________    /:::/    / \:::\    \  
- /:::/\:::\   \:::\____\  /:::/\:::::::::::\    \  /:::/    /   \:::\ ___\ 
-/:::/  \:::\   \:::|    |/:::/  |:::::::::::\____\/:::/____/  ___\:::|    |
-\::/    \:::\  /:::|____|\::/   |::|~~~|~~~~~     \:::\    \ /\  /:::|____|
- \/_____/\:::\/:::/    /  \/____|::|   |           \:::\    /::\ \::/    / 
-          \::::::/    /         |::|   |            \:::\   \:::\ \/____/  
-           \::::/    /          |::|   |             \:::\   \:::\____\    
-            \::/____/           |::|   |              \:::\  /:::/    /    
-             ~~                 |::|   |               \:::\/:::/    /     
-                                |::|   |                \::::::/    /      
-                                \::|   |                 \::::/    /       
-                                 \:|   |                  \::/____/        
-                                  \|___|                                   
-                                                                           
-    )" << '\n';
-
-
-    cout<< "\n \n    Pass Key Generator \n";
-    cout<< "    Version 0.2 \n";
-    cout<< "    This software uses teh GNU General Public License \n";
-    cout<< "    if for whatever reason you need to check out the deets \n";
-    cout<< "    licencing info can be found at https://www.gnu.org/licenses/gpl-3.0.en.html \n";
-    cout<< "\n"<<"    I make no garuantee on this software being either safe to use or even be in a working state\n \n \n";
-
-}
-
-random_device rd;
-
 int main (int argc, char *argv[]) {
-    srand ( time(NULL) );
-    cout << "Debug myChars vector length: " << size(myChars) << "\n";
-    //Check that some params have been passed
-    //will error if no params are given
-	if (argc <= 1)
-	{
-		if (argv[0])
-			cout << "Usage: " << argv[0] << " <number>" << '\n';
-		else
-			cout << "Usage: <program name> <number>" << '\n';
 
-		return 1;
-	}
+    int max {73}; //upper limit of the randomiser
+    srand(time(NULL)); //proper random?
 
-    //converts char to int
-	stringstream convert{ argv[1] }; // set up a stringstream variable named convert, initialized with the input from argv[1]
+    vector <char> myChars {'q','w','e','r','t','y','u','i','o','p','a','s','d','f','g','h','j','k','l','z','x','c','v','b','n',
+    'm','Q','W','E','R','T','Y','U','I','O','P','A','S','D','F','G','H','J','K','L','Z','X','C','V','B','N','M',
+    '!','@','#','$','%','^','&','*','?','<','>','1','2','3','4','5','6','7','8','9','0'};
 
-	int myint{};
-	if (!(convert >> myint)) // do the conversion
-		myint = 0; // if conversion fails, set myint to a default value
+    string partA, partB, partC, partD, partE, partF, partG, partH, partI, partJ, partK, partL, partM, partN;
 
-	cout << "Got integer: " << myint << '\n';
-    for(auto i=1 ; i <= myint ; i++) {
-        auto num = rd() % size(myChars) + 0;
-        passkey.push_back(num);
-        cout << "added "<<num<<"\n";
+    partA = myChars.at(rand()%max);
+    partB = myChars.at(rand()%max);
+    partC = myChars.at(rand()%max);
+    partD = myChars.at(rand()%max);
+    partE = myChars.at(rand()%max);
+    partF = myChars.at(rand()%max);
+    partG = myChars.at(rand()%max);
+    partH = myChars.at(rand()%max);
+    partI = myChars.at(rand()%max);
+    partK = myChars.at(rand()%max);
+    partL = myChars.at(rand()%max);
+    partM = myChars.at(rand()%max);
+    partN = myChars.at(rand()%max);
 
-    }
-    cout <<passkey;
-    for(auto i: passkey)
-    cout<<i;
-
-    if(argc > 1) {
-        for(int i = 0 ; i <= argc ; i++) {
-            if (*argv[i] == 'H'){
-                helpCommand();
-            }
-            if (*argv[i] == 'A'){
-                art();
-            }
-            if (*argv[i] == 'Q') {
-                cout<<"There is an Q in the command"<< endl;
-            } else {
-                
-            }
-        }
-    }
-        
+    cout<<partA<<partB<<partC<<partD<<partE<<partF<<partG<<partH<<partI<<partJ<<partK<<partL<<partM<<partN<<endl;
 
     return 0;
 }
